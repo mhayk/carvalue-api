4c1c57a741afdfa21096946251da1d2e5b9da417
Mhayk Whandson <hi@mhayk.com>
2022-03-02 09:35:53 +0000

    service methos as create, findOne, find and Update have been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,23 @@ export class UsersService {
 
         return this.repo.save(user);
     }
+
+    findOne(id: number) {
+        return this.repo.findOne(id);
+    }
+
+    find(email: string) {
+        return this.repo.find({ email });
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    remove() { }
 }
