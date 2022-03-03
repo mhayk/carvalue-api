437da4a13360bd3bff5b293ebeebb6d15e141856
Mhayk Whandson <hi@mhayk.com>
2022-03-03 10:10:11 +0000

    wrapping the interceptor in a Decorator.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize, } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
@@ -13,7 +13,7 @@ export class UsersController {
         return this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(new SerializeInterceptor(UserDto))
+    @Serialize(UserDto)
     @Get('/:id')
     findUser(@Param('id') id: string) {
         return this.usersService.findOne(parseInt(id));
