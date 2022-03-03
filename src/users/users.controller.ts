14ab1e835c7505c4b6919db648901bd0ebae3b44
Mhayk Whandson <hi@mhayk.com>
2022-03-03 10:51:20 +0000

    controller-wide serialization
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { Serialize, } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
@@ -13,9 +14,13 @@ export class UsersController {
         return this.usersService.create(body.email, body.password);
     }
 
-    @Serialize(UserDto)
     @Get('/:id')
     findUser(@Param('id') id: string) {
         return this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
     }
 }
