25eb73367ded6a4c97d30f7e179202fe436e8512
Mhayk Whandson <hi@mhayk.com>
2022-03-06 21:37:17 +0000

    testing setup
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+it('can create an instance of auth service', async () => {
+  // Create a fake copy of the users service
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) => Promise.resolve({ id: 1, email, password }),
+  }
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      }
+    ],
+  }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+  const service = module.get<AuthService>(AuthService);
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  expect(service).toBeDefined();
+})
